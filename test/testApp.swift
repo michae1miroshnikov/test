36f4c250c20a3203b866d4474b8054ef315224f8
Michael Miroshnikov <mishamiroshnikov054@gmail.com>
2025-08-07 12:37:26 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Michael Miroshnikov on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
